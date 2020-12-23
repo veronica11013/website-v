ec1355ab9585380c3f3aeaf546f93e01f89aab70
veronica11013 <68744490+veronica11013@users.noreply.github.com>
2020-12-23 19:27:59 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function showMobileMenu() {
+  let mobileMenu = document.querySelector("#mobile-menu");
+  mobileMenu.style.display = "block";
+}
+function closeMobileMenu() {
+  let mobileMenu = document.querySelector("#mobile-menu");
+  mobileMenu.style.display = "none";
+}
+function maskOut() {
+  let mask = document.querySelector("#mask");
+  mask.style.display = "none";
+}
